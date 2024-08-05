9dd5378daee3c6d0be1fa0db22d4be6b8aec7810
Brayan Casallas <beto980730@gmail.com>
2024-08-05 18:33:59 -0500

    Actualizacion

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '../components/Presentacion.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('../components/Presentacion.vue')
+      component: Home
     },
     {
       path: '/about',
