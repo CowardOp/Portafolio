e8da719adbc237368dc074288ac3ae254b944914
Brayan Casallas <beto980730@gmail.com>
2024-08-07 19:51:34 -0500

    Actualizacion

---
+++ src/components/botonServicios.vue
@@ -0,0 +1,19 @@
+<template>
+    <a @click="navigateToLink" :class="buttonClass">
+        {{ buttonText }}</a>
+</template>
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+interface Props {
+    link: string;
+    buttonText: string;
+    buttonClass?: string;
+}
+
+const props = defineProps<Props>();
+
+function navigateToLink() {
+    window.location.href = props.link;
+}
+</script>
--- src/components/proyectos.vue
+++ src/components/proyectos.vue
@@ -1,17 +1,34 @@
 <template>
-    <div class="services w-full h-[100vh]">
-        <div>
+    <div class="services flex w-full h-[100vh] pt-5 md:ps-[100px]">
+        <section class="grid grid-cols-1 md:grid-cols-2 gap-4 gap-5 md:h-2/3 md:w-2/3">
 
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
+            <div class="col">
+                <h1>Buenas Tardes</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere numquam voluptas iste libero
+                    cupiditate
+                    officiis ab perspiciatis officia expedita ex, consectetur impedit eum ratione obcaecati nam aut
+                    temporibus quasi ullam!</p>
+                <div>
+                    <botonServicios link="https://www.example.com" buttonText="Ir a Example"
+                        buttonClass="my-custom-class" />
+                    <botonServicios link="https://www.example.com" buttonText="Ir a Example" buttonClass="github" />
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
 
     </div>
 </template>
 
 <script lang="ts" setup>
+import botonServicios from './botonServicios.vue';
 </script>
 
 
-<style scoped></style>
+<style scoped>
+.github {
+    background-color: red;
+}
+</style>
